2fca83181ead3ab2a541619ece8afec78b4dddc1
HoutarouM <marian.horoshko@gmail.com>
2022-10-13 08:49:18 +0200

    one dice

--- app/src/main/java/com/example/kostka/MainActivity.java
+++ app/src/main/java/com/example/kostka/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.kostka;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     public int randomNum;
+    public ArrayList<Integer> diceImageIds = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
         Button diceButton = findViewById(R.id.button);
 
         TextView diceTextView = findViewById(R.id.text_view);
+        ImageView diceImageView = findViewById(R.id.imageView);
+
+        diceImageIds.add(R.drawable.dice_1);
+        diceImageIds.add(R.drawable.dice_2);
+        diceImageIds.add(R.drawable.dice_3);
+        diceImageIds.add(R.drawable.dice_4);
+        diceImageIds.add(R.drawable.dice_5);
+        diceImageIds.add(R.drawable.dice_6);
 
         diceButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
 //                set random number to text view
                 diceTextView.setText(Integer.toString(randomNum));
+
+                diceImageView.setImageResource(diceImageIds.get(randomNum - 1));
             }
         });
     }
